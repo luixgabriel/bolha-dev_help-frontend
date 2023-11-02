5c50f9b99142fcfbf2ecb53e16fb890cfa1de02d
Luis Gabriel <luis16757@gmail.com>
2023-11-02 19:35:26 -0300

    fix: aaaaaaa

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -2,19 +2,24 @@ import { NextRequest, NextResponse } from 'next/server'
 import axios from '../../../../services/axios'
 
 export async function GET(request: NextRequest) {
-  return NextResponse.json({ msg: 'FUNCINOA PÓRRA' })
-  // const { searchParams } = new URL(request.url)
-  // const code = searchParams.get('code')
-  // const redirectTo = request.cookies.get('redirectTo')?.value
-  // const registerResponse = await axios.post('/auth/github', {
-  //   code,
-  // })
-  // const { token } = registerResponse.data
-  // const redirectURL = redirectTo ?? new URL('/', request.url)
-  // const cookieExpiresInSeconds = 60 * 60 * 24 * 30
-  // return NextResponse.redirect(redirectURL, {
-  //   headers: {
-  //     'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
-  //   },
-  // })
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  const redirectTo = request.cookies.get('redirectTo')?.value
+
+  const registerResponse = await axios.post('/auth/github', {
+    code,
+  })
+
+  const { token } = registerResponse.data
+
+  const redirectURL = redirectTo ?? new URL('/', request.url)
+
+  const cookieExpiresInSeconds = 60 * 60 * 24 * 30
+
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
+    },
+  })
 }
