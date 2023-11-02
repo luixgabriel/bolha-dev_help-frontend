72ba80bc6de786cd12919fad4c2146c487cfc95f
Luis Gabriel <luis16757@gmail.com>
2023-11-02 19:30:37 -0300

    fix: add imageUrl in token

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -2,24 +2,19 @@ import { NextRequest, NextResponse } from 'next/server'
 import axios from '../../../../services/axios'
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
-  const code = searchParams.get('code')
-
-  const redirectTo = request.cookies.get('redirectTo')?.value
-
-  const registerResponse = await axios.post('/auth/github', {
-    code,
-  })
-
-  const { token } = registerResponse.data
-
-  const redirectURL = redirectTo ?? new URL('/', request.url)
-
-  const cookieExpiresInSeconds = 60 * 60 * 24 * 30
-
-  return NextResponse.redirect(redirectURL, {
-    headers: {
-      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
-    },
-  })
+  NextResponse.json({ msg: 'FUNCINOA PÓRRA' })
+  // const { searchParams } = new URL(request.url)
+  // const code = searchParams.get('code')
+  // const redirectTo = request.cookies.get('redirectTo')?.value
+  // const registerResponse = await axios.post('/auth/github', {
+  //   code,
+  // })
+  // const { token } = registerResponse.data
+  // const redirectURL = redirectTo ?? new URL('/', request.url)
+  // const cookieExpiresInSeconds = 60 * 60 * 24 * 30
+  // return NextResponse.redirect(redirectURL, {
+  //   headers: {
+  //     'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
+  //   },
+  // })
 }
