100eab44127a51b02c9ea298108a7dafa9236293
Luis Gabriel <luis16757@gmail.com>
2023-11-15 08:10:21 -0300

    feat: style answers section

--- src/app/doubts/page.tsx
+++ src/app/doubts/page.tsx
@@ -2,7 +2,13 @@
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import Image from 'next/image'
-import { CircuitBoard, CornerDownRight, CornerLeftDown } from 'lucide-react'
+import {
+  CircuitBoard,
+  CornerDownRight,
+  CornerLeftDown,
+  MessagesSquare,
+  ThumbsUp,
+} from 'lucide-react'
 import testImg from '../../assets/imgs/carbon.png'
 import doubts from '../../data/doubts'
 import { useDoubtsDataById } from '../../hooks/useDoubtsById'
@@ -13,7 +19,7 @@ const Doubts = ({ searchParams }: { searchParams: { id: string } }) => {
   const { data } = useDoubtsDataById(searchParams.id)
   const exampleDoubt = doubts[3]
   const date = new Date(exampleDoubt.createdAt)
-  const formatoAmigavel = formatDistanceToNow(date, {
+  const formatDate = formatDistanceToNow(date, {
     addSuffix: true,
     locale: ptBR,
   })
@@ -30,7 +36,7 @@ const Doubts = ({ searchParams }: { searchParams: { id: string } }) => {
           />
           <span>{exampleDoubt.user.name}</span>
         </div>
-        <span>{formatoAmigavel}</span>
+        <span>{formatDate}</span>
       </div>
       <div className="mt-2 p-2 w-screen">
         <span className="text-sm text-gray-400 pl-1 flex items-center gap-1">
@@ -50,14 +56,42 @@ const Doubts = ({ searchParams }: { searchParams: { id: string } }) => {
         placeholder="Responder"
         className="block w-[95%] self-center rounded-md border-0 mt-3 py-1.5 px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black  focus:ring-2 focus:ring-inset"
       />
-      <div className="p-2 m-3 flex gap-2">
-        <CornerLeftDown className="mt-2" />
+      <div className="p-2 mt-3 mx-1 flex gap-2">
+        <MessagesSquare />
         <span>50 Respostas</span>
       </div>
       <div className="w-[95%] mx-auto">
         {answers.map((item: IAnswers, index) => (
-          <div key={index} className="bg-red-400 m-2">
-            <h1>{item.description}</h1>
+          <div key={index} className="shadow-md rounded-md m-3">
+            <div className="flex px-2 justify-between">
+              <div className="mb-3">
+                <h1>{item.user.name}</h1>
+                <span className="text-xs overflow-y-hidden">
+                  {formatDistanceToNow(new Date(item.createdAt), {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </span>
+                <div className="h-[1px] bg-black w-full" />
+              </div>
+              <span className="flex gap-2 m-3">
+                {item.likes}
+                <ThumbsUp size={20} />
+              </span>
+            </div>
+            <p className="px-1 my-2 mb-3">
+              HTML, que significa HyperText Markup Language (Linguagem de
+              Marcação de Hipertexto, em português), é a linguagem padrão
+              utilizada para criar páginas web. Foi desenvolvida para ser
+              interpretada por navegadores web, permitindo a estruturação de
+              conteúdo como texto, imagens, links, formulários e outros
+              elementos.
+            </p>
+            <div className="flex justify-center gap-5 my-3">
+              <span>Curtir</span>
+              <span>Comentar</span>
+              <span>Enviar mensagem</span>
+            </div>
           </div>
         ))}
       </div>
