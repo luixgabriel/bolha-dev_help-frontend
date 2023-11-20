22ec29b694e67503560d7da35e75c419e2e730ad
Luis Gabriel <luis16757@gmail.com>
2023-11-20 19:05:28 -0300

    feat: add likes in comment

--- src/app/components/comment-list.tsx
+++ src/app/components/comment-list.tsx
@@ -1,6 +1,6 @@
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
-import { CornerDownRightIcon } from 'lucide-react'
+import { CornerDownRightIcon, ThumbsUp } from 'lucide-react'
 import React from 'react'
 
 const CommentList = ({ comment }: any) => {
@@ -8,19 +8,25 @@ const CommentList = ({ comment }: any) => {
     <>
       {comment?.map((item: any) => (
         <div key={item.id} className="flex flex-col py-2">
-          <div className="flex mx-1 gap-2">
-            {' '}
-            <CornerDownRightIcon />
-            <div>
-              <h1 className="text-sm font-semibold">{item.user.name}</h1>
-              <span className="text-xs overflow-y-hidden">
-                {formatDistanceToNow(new Date(item.createdAt), {
-                  addSuffix: true,
-                  locale: ptBR,
-                })}
-              </span>
+          <div className="flex justify-between">
+            <div className="flex mx-1 gap-2">
+              {' '}
+              <CornerDownRightIcon />
+              <div>
+                <h1 className="text-sm font-semibold">{item.user.name}</h1>
+                <span className="text-xs overflow-y-hidden">
+                  {formatDistanceToNow(new Date(item.createdAt), {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </span>
+              </div>
             </div>
+            <span className="ml-1 flex items-center gap-2">
+              10 <ThumbsUp className="cursor-pointer" size={18} />
+            </span>
           </div>
+
           <span className="ml-7 mt-1 w-[90%] mb-4 border border-gray-500 rounded-lg p-2">
             {item.content}
           </span>
