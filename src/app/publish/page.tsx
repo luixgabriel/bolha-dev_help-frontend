b74716fb632f55240c2a6daec4921f7eba94dea2
Luis Gabriel <luis16757@gmail.com>
2023-12-19 19:35:21 -0300

    chore: test publish page again

--- src/app/publish/page.tsx
+++ src/app/publish/page.tsx
@@ -43,73 +43,80 @@ const PublishDoubt = () => {
   }
 
   return (
-    <div className={`${darkMode && 'bg-blak'} h-screen w-screen`}>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className={` mx-auto h-screen w-screen ${
+    <div
+      className={`${
+        darkMode && 'bg-blak'
+      } h-screen w-screen flex items-center justify-center`}
+    >
+      <div
+        className={`overflow-y-hidden lg:w-1/2 ${
           darkMode ? 'bg-blak ' : 'bg-white'
-        } p-8 shadow-md rounded`}
+        } p-2 shadow-md rounded`}
       >
-        <div className="mb-4">
-          <label className="block text-gray-600">Título:</label>
-          <input
-            {...register('title', { required: 'Campo obrigatório' })}
-            type="text"
-            className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black  focus:ring-2 focus:ring-inset focus:outline-none"
-          />
-          <span className="text-red-500 text-sm">{errors.title?.message}</span>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
+            <label className="block text-gray-600">Título:</label>
+            <input
+              {...register('title', { required: 'Campo obrigatório' })}
+              type="text"
+              className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black  focus:ring-2 focus:ring-inset focus:outline-none"
+            />
+            <span className="text-red-500 text-sm">
+              {errors.title?.message}
+            </span>
+          </div>
 
-        <div className="mb-4">
-          <label className="block text-gray-600">Categoria:</label>
-          <Controller
-            render={({ field }) => (
-              <select
-                {...field}
-                defaultValue={categoryList[0].name}
-                className="form-select mt-1 block w-full p-2 border rounded-md shadow-sm focus:outline-none"
-              >
-                {categoryList.map((item: ICategory) => (
-                  <option value={item.name} key={item.id}>
-                    {item.name}
-                  </option>
-                ))}
-              </select>
-            )}
-            control={control}
-            name="category"
-          />
-        </div>
+          <div className="mb-4">
+            <label className="block text-gray-600">Categoria:</label>
+            <Controller
+              render={({ field }) => (
+                <select
+                  {...field}
+                  defaultValue={categoryList[0].name}
+                  className="form-select mt-1 block w-full p-2 border rounded-md shadow-sm focus:outline-none"
+                >
+                  {categoryList.map((item: ICategory) => (
+                    <option value={item.name} key={item.id}>
+                      {item.name}
+                    </option>
+                  ))}
+                </select>
+              )}
+              control={control}
+              name="category"
+            />
+          </div>
 
-        <div className="mb-4 ">
-          <label className="block text-gray-600">Descrição:</label>
-          <textarea
-            {...register('description', { required: 'Campo obrigatório' })}
-            rows={6}
-            className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black  focus:ring-2 focus:ring-inset focus:outline-none"
-          />
-          <span className="text-red-500 text-sm">
-            {errors.description?.message}
-          </span>
-        </div>
-        <div className="my-4">
-          <span className="text-sm leading-3 pl-2 text-gray-400">
-            A imagem é opcional!
-          </span>
-          <FileInput onFileChanges={handleFileChange} />
-        </div>
+          <div className="mb-4 ">
+            <label className="block text-gray-600">Descrição:</label>
+            <textarea
+              {...register('description', { required: 'Campo obrigatório' })}
+              rows={6}
+              className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black  focus:ring-2 focus:ring-inset focus:outline-none"
+            />
+            <span className="text-red-500 text-sm">
+              {errors.description?.message}
+            </span>
+          </div>
+          <div className="my-4">
+            <span className="text-sm leading-3 pl-2 text-gray-400">
+              A imagem é opcional!
+            </span>
+            <FileInput onFileChanges={handleFileChange} />
+          </div>
 
-        <button
-          type="submit"
-          className={`flex w-full justify-center rounded-md ${
-            darkMode
-              ? 'bg-gray-200 text-black hover:bg-gray-300'
-              : 'bg-blak text-white hover:bg-black'
-          } px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
-        >
-          {isPending ? <LoadingIcon /> : 'Enviar'}
-        </button>
-      </form>
+          <button
+            type="submit"
+            className={`flex w-full justify-center rounded-md ${
+              darkMode
+                ? 'bg-gray-200 text-black hover:bg-gray-300'
+                : 'bg-blak text-white hover:bg-black'
+            } px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+          >
+            {isPending ? <LoadingIcon /> : 'Enviar'}
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
