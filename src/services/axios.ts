ae4f130d1a466b7329f66fb006eaed9380480938
Luis Gabriel <luis16757@gmail.com>
2024-01-03 13:23:55 -0300

    fix: dark mode in selected doubt

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,7 +2,4 @@ import axios from 'axios'
 // https://bolhadev-help.onrender.com
 export default axios.create({
   baseURL: 'https://bolhadev-help.onrender.com',
-  // headers: {
-  //   Authorization: ` Bearer ${process.env.NEXT_PUBLIC_GITHUB_KEY} `,
-  // },
 })
