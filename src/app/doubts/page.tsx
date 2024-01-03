f82e4bd24a7fa65e54c65e45195fab9d1e207e5e
Luis Gabriel <luis16757@gmail.com>
2024-01-03 10:53:09 -0300

    fix: testing bug

--- src/app/doubts/page.tsx
+++ src/app/doubts/page.tsx
@@ -23,44 +23,46 @@ const Doubts = ({ searchParams }: { searchParams: { id: string } }) => {
     )
   }
   return (
-    <div
-      className={`w-screen flex flex-col overflow-y-hidden h-screen ${
-        darkMode ? 'bg-blak text-white' : 'bg-white text-black'
-      }`}
-    >
-      <div className="flex items-center gap-3 p-3 justify-between mt-4 w-[97%] shadow-md self-center rounded-md overflow-y-hidden">
-        <div className="flex items-center gap-3">
-          <Image
-            src={
-              data.user.imageUrl ? (data.user.imageUrl as string) : defaultImg
-            }
-            width={50}
-            height={50}
-            alt="user-image"
-            className="rounded-full"
-          />
-          <span>{data.user.name}</span>
+    <section className="bg-blak sm:h-screen">
+      <div
+        className={`w-screen flex flex-col overflow-y-hidden  ${
+          darkMode ? 'bg-blak text-white' : 'bg-white text-black'
+        }`}
+      >
+        <div className="flex items-center gap-3 p-3 justify-between mt-4 w-[97%] shadow-md self-center rounded-md overflow-y-hidden">
+          <div className="flex items-center gap-3">
+            <Image
+              src={
+                data.user.imageUrl ? (data.user.imageUrl as string) : defaultImg
+              }
+              width={50}
+              height={50}
+              alt="user-image"
+              className="rounded-full"
+            />
+            <span>{data.user.name}</span>
+          </div>
+          <span>
+            {formatDistanceToNow(new Date(data.createdAt), {
+              addSuffix: true,
+              locale: ptBR,
+            })}
+          </span>
         </div>
-        <span>
-          {formatDistanceToNow(new Date(data.createdAt), {
-            addSuffix: true,
-            locale: ptBR,
-          })}
-        </span>
+        <DoubtContent
+          category={data.category}
+          description={data.description}
+          title={data.title}
+          image={data.image as string}
+        />
+        <TextAreaAnswer doubtsId={data.id} />
+        <div className="p-2 mt-1 mx-1 flex gap-2">
+          <MessagesSquare />
+          <span>{data.Answers?.length} Respostas</span>
+        </div>
+        <AnswersList answers={data.Answers} />
       </div>
-      <DoubtContent
-        category={data.category}
-        description={data.description}
-        title={data.title}
-        image={data.image as string}
-      />
-      <TextAreaAnswer doubtsId={data.id} />
-      <div className="p-2 mt-1 mx-1 flex gap-2">
-        <MessagesSquare />
-        <span>{data.Answers?.length} Respostas</span>
-      </div>
-      <AnswersList answers={data.Answers} />
-    </div>
+    </section>
   )
 }
 
