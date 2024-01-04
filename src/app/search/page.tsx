4846dbc73afd4982b5eb42e1239b05d4f42dface
Luis Gabriel <luis16757@gmail.com>
2024-01-04 19:51:02 -0300

    feat: add darkMode in search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,13 +6,19 @@ import defaultImg from '../../assets/imgs/null.png'
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { useRouter } from 'next/navigation'
+import { useDarkMode } from '../../hooks/useDarkMode'
 
 const Search = ({ searchParams }: { searchParams: { filter: string } }) => {
   const { data, isLoading } = useDoubtsBySearch(searchParams.filter)
+  const { darkMode } = useDarkMode()
   const router = useRouter()
   return (
-    <div className="h-screen flex">
-      <div className="w-full p-4 bg-white rounded-md shadow-md">
+    <div className="h-screen w-screen flex ">
+      <div
+        className={`w-full p-4 ${
+          darkMode ? 'bg-blak text-white' : 'bg-white'
+        } shadow-md`}
+      >
         <h1 className="text-2xl font-bold mb-4 overflow-y-hidden">
           Resultados Encontrados
         </h1>
@@ -28,7 +34,9 @@ const Search = ({ searchParams }: { searchParams: { filter: string } }) => {
           data.map((item: IDoubts) => (
             <div
               key={item.id}
-              className="mb-4 p-4 border rounded-md cursor-pointer hover:bg-gray-100"
+              className={`mb-4 p-4 border rounded-md cursor-pointer ${
+                darkMode ? 'hover:bg-gray-950' : 'hover:bg-gray-100'
+              }`}
               onClick={() => router.push(`doubts?id=${item.id}`)}
             >
               <div className="flex items-center mb-2">
@@ -41,7 +49,11 @@ const Search = ({ searchParams }: { searchParams: { filter: string } }) => {
                 />
                 <div className="flex flex-col ml-2">
                   <h1 className="text-lg font-bold">{item.title}</h1>
-                  <p className="text-gray-500 text-sm">
+                  <p
+                    className={`${
+                      darkMode ? 'text-gray-100' : 'text-gray-500'
+                    } text-sm`}
+                  >
                     {' '}
                     {formatDistanceToNow(new Date(item.createdAt), {
                       addSuffix: true,
@@ -50,7 +62,13 @@ const Search = ({ searchParams }: { searchParams: { filter: string } }) => {
                   </p>
                 </div>
               </div>
-              <p className="text-gray-700 text-sm">{item.description}</p>
+              <p
+                className={`${
+                  darkMode ? 'text-gray-100' : 'text-gray-700'
+                } text-sm`}
+              >
+                {item.description}
+              </p>
               <div className="flex items-center mt-2">
                 <span className="text-gray-500 text-sm">
                   Autor: {item.user.name}
