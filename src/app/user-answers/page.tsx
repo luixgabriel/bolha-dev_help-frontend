af1bf067781b20f7da2e53f53d52916e1e12f721
Luis Gabriel <luis16757@gmail.com>
2024-01-05 10:34:50 -0300

    fix: send a message when user not send a doubt or a answer

--- src/app/user-answers/page.tsx
+++ src/app/user-answers/page.tsx
@@ -57,94 +57,108 @@ const UserAnswers = ({ searchParams }: { searchParams: { id: string } }) => {
   return (
     <>
       <div className={`${darkMode && 'bg-blak'} w-screen h-screen`}>
-        <div className="my-5 flex flex-col items-center">
-          {data.map((item: IUserAnswers) => (
-            <div
-              key={item.id}
-              className={`flex gap-2 p-2 rounded-lg ${
-                darkMode ? 'bg-gray-950 text-white' : 'bg-gray-300  text-black'
-              } items-center justify-center w-[90%] my-1`}
+        {data?.length === 0 ? (
+          <div className="p-5 flex items-center justify-center">
+            <h1
+              className={`text-2xl overflow-y-hidden ${
+                darkMode ? 'text-white' : 'text-black'
+              }`}
             >
-              <Image
-                src={
-                  item.user.imageUrl
-                    ? (item.user.imageUrl as string)
-                    : defaultImg
-                }
-                width={40}
-                height={40}
-                alt="user-icon"
-                className="rounded-full cursor-pointer"
-              />
-              <p className="w-[60%]">
-                {(screenWidht as number) < 700
-                  ? truncateText(item.description, 35)
-                  : item.description}
-              </p>
-              <div className="flex gap-2 items-center">
-                <DeleteModalAnswers
-                  answerId={item.id}
-                  show={showModal}
-                  onConfirm={confirmDelete}
-                  onCancel={cancelDelete}
+              Você ainda não enviou nenhuma resposta.
+            </h1>
+          </div>
+        ) : (
+          <div className="my-5 flex flex-col items-center">
+            {data.map((item: IUserAnswers) => (
+              <div
+                key={item.id}
+                className={`flex gap-2 p-2 rounded-lg ${
+                  darkMode
+                    ? 'bg-gray-950 text-white'
+                    : 'bg-gray-300  text-black'
+                } items-center justify-center w-[90%] my-1`}
+              >
+                <Image
+                  src={
+                    item.user.imageUrl
+                      ? (item.user.imageUrl as string)
+                      : defaultImg
+                  }
+                  width={40}
+                  height={40}
+                  alt="user-icon"
+                  className="rounded-full cursor-pointer"
                 />
-                <span className="flex gap-1">
-                  <ThumbsUp />
-                  {item.likes}
-                </span>
-                <span className="flex gap-1">
-                  <MessageSquare
-                    onClick={() => handleNavigate(item.doubts.id)}
-                    className="cursor-pointer"
+                <p className="w-[60%]">
+                  {(screenWidht as number) < 700
+                    ? truncateText(item.description, 35)
+                    : item.description}
+                </p>
+                <div className="flex gap-2 items-center">
+                  <DeleteModalAnswers
+                    answerId={item.id}
+                    show={showModal}
+                    onConfirm={confirmDelete}
+                    onCancel={cancelDelete}
                   />
-                  {item.Comment?.length}
-                </span>
-                {(screenWidht as number) < 700 ? (
-                  <>
-                    {' '}
-                    <Pencil
-                      onClick={() => handleNavigateToEdit(item.id)}
-                      className="cursor-pointer"
-                    />
-                    <X
-                      onClick={() => handleDelete()}
-                      className="cursor-pointer"
-                    />
-                  </>
-                ) : (
-                  <>
-                    <ArrowRight
+                  <span className="flex gap-1">
+                    <ThumbsUp />
+                    {item.likes}
+                  </span>
+                  <span className="flex gap-1">
+                    <MessageSquare
                       onClick={() => handleNavigate(item.doubts.id)}
                       className="cursor-pointer"
                     />
-                    <button
-                      onClick={() => handleNavigateToEdit(item.id)}
-                      className={`${
-                        darkMode
-                          ? 'bg-green-500 hover:bg-green-600'
-                          : 'bg-green-300 hover:bg-green-400'
-                      } p-2 rounded-md shadow-md flex justify-center items-center gap-1`}
-                    >
-                      Editar
-                      <Pencil size={17} />
-                    </button>
-                    <button
-                      onClick={() => handleDelete()}
-                      className={`${
-                        darkMode
-                          ? 'bg-red-500 hover:bg-red-600'
-                          : 'bg-red-300 hover:bg-red-400'
-                      } p-2 rounded-md shadow-md flex justify-between items-center gap-1`}
-                    >
-                      Deletar
-                      <X size={17} />
-                    </button>
-                  </>
-                )}
+                    {item.Comment?.length}
+                  </span>
+                  {(screenWidht as number) < 700 ? (
+                    <>
+                      {' '}
+                      <Pencil
+                        onClick={() => handleNavigateToEdit(item.id)}
+                        className="cursor-pointer"
+                      />
+                      <X
+                        onClick={() => handleDelete()}
+                        className="cursor-pointer"
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <ArrowRight
+                        onClick={() => handleNavigate(item.doubts.id)}
+                        className="cursor-pointer"
+                      />
+                      <button
+                        onClick={() => handleNavigateToEdit(item.id)}
+                        className={`${
+                          darkMode
+                            ? 'bg-green-500 hover:bg-green-600'
+                            : 'bg-green-300 hover:bg-green-400'
+                        } p-2 rounded-md shadow-md flex justify-center items-center gap-1`}
+                      >
+                        Editar
+                        <Pencil size={17} />
+                      </button>
+                      <button
+                        onClick={() => handleDelete()}
+                        className={`${
+                          darkMode
+                            ? 'bg-red-500 hover:bg-red-600'
+                            : 'bg-red-300 hover:bg-red-400'
+                        } p-2 rounded-md shadow-md flex justify-between items-center gap-1`}
+                      >
+                        Deletar
+                        <X size={17} />
+                      </button>
+                    </>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
       <Footer />
     </>
